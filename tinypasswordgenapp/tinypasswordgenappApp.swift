5fb81181e654f07b3101d998605dea77aef3c3e4
Corrado Ignoti <corrado@corradoignoti.it>
2024-04-24 12:55:10 +0200

    Initial Commit

---
+++ tinypasswordgenapp/tinypasswordgenappApp.swift
@@ -0,0 +1,17 @@
+//
+//  tinypasswordgenappApp.swift
+//  tinypasswordgenapp
+//
+//  Created by corrado.ignoti on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct tinypasswordgenappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
